ed1065888dc763cc16ac074152f4806f2b9fafba
Angelo Genovese <angelo@genovese.ca>
2015-09-17 19:30:38 -0400

    Completed Monad typeclass and associated laws

---
+++ src/test/java/ca/genovese/coffeecats/structures/MonadLaws.java
@@ -0,0 +1,46 @@
+package ca.genovese.coffeecats.structures;
+
+import ca.genovese.coffeecats.util.Kind;
+
+import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
+
+public interface MonadLaws<A, B, C, F extends Kind> extends ApplicativeLaws<A, B, C, F> {
+
+  Monad<F> getMonad();
+
+  @Override
+  default Applicative<F> getApplicative() {
+    return getMonad();
+  }
+
+  Function<A, Kind<F, B>> getRandomAFB();
+
+  void testMonadLeftIdentity();
+
+  default void monadLeftIdentity() {
+    for (int i = 0; i < getExecutionCount(); i++) {
+      A a = getRandomA();
+      Function<A, Kind<F, B>> f = getRandomAFB();
+      //F.pure(a).flatMap(f) <-> f(a)
+      assertEquals(
+          "flatmapping a function over the pure of a value " + "should be equal to applying the function to the value",
+          getMonad().flatMap(getMonad().pure(a), f),
+          f.apply(a));
+    }
+  }
+
+  void testMonadRightIdentity();
+
+  default void monadRightIdentity() {
+    for (int i = 0; i < getExecutionCount(); i++) {
+      Kind<F, A> fa = getRandomFA();
+
+      assertEquals(
+          "flatmapping a function over the pure of a value " + "should be equal to applying the function to the value",
+          getMonad().flatMap(fa, a -> getMonad().pure(a)),
+          fa);
+    }
+  }
+}
