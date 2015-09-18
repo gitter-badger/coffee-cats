3bc1201fb3f9016bc50e16f065b21f1d9c37ede8
Angelo Genovese <angelo@genovese.ca>
2015-09-18 09:59:18 -0400

    Added the Comonad typeclass and associated laws

---
+++ src/main/java/ca/genovese/coffeecats/structures/Comonad.java
@@ -0,0 +1,10 @@
+package ca.genovese.coffeecats.structures;
+
+import ca.genovese.coffeecats.util.Kind;
+
+/**
+ * Must obey the laws defined in [[laws.ComonadLaws]].
+ */
+public interface Comonad<F extends Kind> extends CoflatMap<F> {
+  <A> A extract(Kind<F, A> fa);
+}
