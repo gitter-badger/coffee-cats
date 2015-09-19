244f69baaad857eac1a93602612cfe56f338f2f9
Angelo Genovese <angelo@genovese.ca>
2015-09-19 18:27:58 -0400

    Added Show typeclass

---
+++ src/main/java/ca/genovese/coffeecats/structures/Show.java
@@ -0,0 +1,21 @@
+package ca.genovese.coffeecats.structures;
+
+import java.util.function.Function;
+
+public interface Show<T> {
+  /** creates an instance of [[Show]] using the provided function */
+  static <A> Show<A> show(Function<A, String> f) {
+    return new Show<A>() {
+      public String show(A a) {
+        return f.apply(a);
+      }
+    };
+  }
+
+  /** creates an instance of [[Show]] using object toString */
+  static <A> Show<A> fromToString() {
+    return a -> a.toString();
+  }
+
+  String show(T t);
+}
