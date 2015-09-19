db6f20300b9a80b759202ad3dbaa3e06647c0673
Angelo Genovese <angelo@genovese.ca>
2015-09-19 18:42:08 -0400

    Added MonadCombine typeclass and associated laws

---
+++ src/main/java/ca/genovese/coffeecats/structures/MonadCombine.java
@@ -0,0 +1,19 @@
+package ca.genovese.coffeecats.structures;
+
+import ca.genovese.coffeecats.util.Kind;
+
+/**
+ * The combination of a Monad with a MonoidK
+ */
+public interface MonadCombine<F extends Kind> extends MonadFilter<F>, Alternative<F> {
+  /**
+   * fold over the inner structure to combining all the values with
+   * our combine method inherited from MonoidK. The result is for us
+   * to accumulate all of the "interesting" values of the innner G, so
+   * if G is Option, we collect all the Some values, if G is Xor,
+   * we collect all the Right values, etc.
+   */
+  default <G extends Kind, A> Kind<F, A> unite(Kind<F, Kind<G, A>> fga, Foldable<G> g) {
+    return flatMap(fga, ga -> g.foldLeft(ga, empty(), (acc, a) -> combine(acc, pure(a))));
+  }
+}
