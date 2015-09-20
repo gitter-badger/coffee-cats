8d2cd6f457950c775e3977c9ca8fcac8a01c7c85
Angelo Genovese <angelo@genovese.ca>
2015-09-20 16:10:11 -0400

    Remove redundant interfaces

--- src/main/java/ca/genovese/coffeecats/util/types/function/Function1.java
+++
@@ -1,7 +0,0 @@
-package ca.genovese.coffeecats.util.types.function;
-
-import java.util.function.Function;
-
-public interface Function1<A, R> extends Function<A, R> {
-
-}
--- src/main/java/ca/genovese/coffeecats/util/types/function/Function3.java
+++ src/main/java/ca/genovese/coffeecats/util/types/function/Function3.java
@@ -2,6 +2,8 @@ package ca.genovese.coffeecats.util.types.function;
 
 import ca.genovese.coffeecats.util.types.tuple.Tuple3;
 
+import java.util.function.Function;
+
 public interface Function3<A, B, C, R> {
   /**
    * Applies this function to the given argument.
@@ -12,7 +14,7 @@ public interface Function3<A, B, C, R> {
    */
   R apply(A a, B b, C c);
 
-  default Function1<Tuple3<A, B, C>, R> tupled() {
+  default Function<Tuple3<A, B, C>, R> tupled() {
     return t -> apply(t.a, t.b, t.c);
   }
 }
